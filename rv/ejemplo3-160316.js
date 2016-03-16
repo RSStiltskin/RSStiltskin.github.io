427db5d2849b989bffc63dddde2a4fe07238e9ba
RSStiltskin <lvegacor@hotmail.com>
2016-03-16 10:57:51 -0600

    Update ejemplo3-160316.js

--- rv/ejemplo3-160316.js
+++ rv/ejemplo3-160316.js
@@ -4,9 +4,17 @@ var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures
 var material = new THREE.MeshBasicMaterial({map: textura});
 var forma = new THREE.BoxGeometry(1, 1, 1);
 malla = new THREE.Mesh(forma,material);
+malla.scale.set(1,4,9);
+
+var luzPuntual = new THREE.PointLight(0xFFFFFF);
+luzPuntual.position.x = 10;
+luzPuntual.position.y = 10;
+luzPuntual.position.z = 10;
 
 escena = new THREE.Scene();
-escena .add(malla);
+escena.add(malla);
+escena.add(luzPuntual);
+
 camara= new THREE.PerspectiveCamera();
 camara.position.z=5;
 
@@ -14,6 +22,7 @@ renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderer.domElement);
 }
+
 function loop(){
 requestAnimationFrame(loop);
 malla.rotation.x += 0.01;
