e1fdc47d774cf55cd701ea3ada7d3e9575aeb4bf
RSStiltskin <lvegacor@hotmail.com>
2016-05-29 12:23:12 -0500

    Update spiderleg-top.js

--- rv/spiderleg-top.js
+++ rv/spiderleg-top.js
@@ -1,16 +1,15 @@
 function setup(){
+  var scene = new THREE.Scene();
+  
+  var camera = new THREE.PerspectiveCamera();
+  camera.position.z=50;
+  
   var shape = new THREE.CylinderGeometry(10,10,20,15,10);
   var material = new THREE.MeshBasicMaterial({color: 0x31B404, wireframe: true});
-  
   var topleg = new THREE.Mesh(shape, material);
-  
-  camera= new THREE.PerspectiveCamera();
-  camera.position.z=50;
-
-  scene = new THREE.Scene();
   scene.add(topleg);
   
-  renderer = new THREE.WebGLRenderer();
+  var renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth,window.innerHeight);
   document.body.appendChild(renderer.domElement);
 }
@@ -19,7 +18,7 @@ function loop(){
   requestAnimationFrame(loop);
   //topleg.rotation.x += 0.01;
   topleg.rotation.y +=0.01;
-  renderer.render(scene, camera);
+  renderer.loop(scene, camera);
 }
 
 setup();
