dfb2e32409b4ce6e604bc9043563c510925d7bdd
RSStiltskin <lvegacor@hotmail.com>
2016-05-30 16:10:35 -0500

    Update Spidey_v3.js

--- rv/Spidey_v3.js
+++ rv/Spidey_v3.js
@@ -10,26 +10,11 @@ this.unionleg = new THREE.Mesh( new THREE.SphereGeometry(8,20,20));
 this.topleg = new THREE.Mesh( new THREE.CylinderGeometry(8,4,16,15,10));
 this.topleg.rotation.z=-2*Math.PI/3;
 this.topleg.position.set(-8*Math.PI/6,8*Math.PI/6,0);
-this.add(this.topleg);
 this.add(this.unionleg);
+this.add(this.topleg);
 }
 
 TpLft.prototype = new THREE.Object3D;
-
-//function Spidey(){
-//THREE.Object3D.call(this);  
-//this.Spbody = new THREE.Mesh( new THREE.Sphere(20,20,20,0,Math.PI) );
-//this.piernaD = new Pierna();
-//this.piernaI = new Pierna();
-//this.cuerpo.position.y=2;
-//this.piernaD.position.z=-1;
-//this.piernaI.position.z=1;
-//this.add(this.cuerpo);
-//this.add(this.piernaD);
-//this.add(this.piernaI);
-//}
-
-//Completo.prototype = new THREE.Object3D;
 
 function setup(){
 body = new TpLft;
@@ -37,7 +22,7 @@ step = .01;
 scene = new THREE.Scene();
 scene.add(body);
 camera = new THREE.PerspectiveCamera();
-camera.position.z=40;
+camera.position.z=100;
 
 renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerHeight*.95, window.innerHeight*.95);
@@ -47,13 +32,6 @@ document.body.appendChild(renderer.domElement);
 function loop(){
 requestAnimationFrame( loop );
 renderer.render (scene, camera);
-
-//if (Math.abs(body.piernaD.rotation.z) > .5 )
-  //step = -step;
-//body.piernaD.rotation.z += step;
-//body.piernaI.rotation.z -= step;
-
-//body.rotation.z += 0.01;
 }
 
 
