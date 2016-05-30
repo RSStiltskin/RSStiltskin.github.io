0fa71cc903551794ecfb92f1bdb02baab394a23f
RSStiltskin <lvegacor@hotmail.com>
2016-05-30 09:35:31 -0500

    Update Spiderleg_v2.js

--- rv/Spiderleg_v2.js
+++ rv/Spiderleg_v2.js
@@ -8,47 +8,34 @@ function setup(){
   far = 1000;
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.z=100;
-  
-  var axisHelper = new THREE.AxisHelper( 40 );
-  //scene.add( axisHelper );
-  
-  var size = 40;
-  var step = 1; 
-  
+
   var rotation = new THREE.Matrix4().makeRotationY(Math.PI/2);
   //rotation.set
   var shbody = new THREE.SphereGeometry(20,20,20,0,Math.PI);
   var mtbody = new THREE.MeshBasicMaterial({color: 0xB40404, wireframe: true});
   spbody = new THREE.Mesh(shbody, mtbody);
-  
   var shape = new THREE.CylinderGeometry(5,4,20,15,10);
   var shape2 = new THREE.CylinderGeometry(4,5,20,15,10);
   var material = new THREE.MeshBasicMaterial({color: 0x31B404, wireframe: true});
-  
   topleg6 = new THREE.Mesh(shape, material);
   topleg6.rotation.z = Math.PI/3;
   topleg6.position.set(30*Math.cos(Math.PI/6),-30*Math.sin(Math.PI/6),0);
-  
   topleg5 = new THREE.Mesh(shape, material);
   topleg5.rotation.z = Math.PI/2;
   topleg5.position.set(30,0,0);
-  
   topleg4 = new THREE.Mesh(shape, material);
   topleg4.rotation.z = 2*Math.PI/3;
   topleg4.position.set(30*Math.cos(Math.PI/6),30*Math.sin(Math.PI/6),0);
-  
   topleg3 = new THREE.Mesh(shape2, material);
   topleg3.rotation.z = 2*Math.PI/3;
   topleg3.position.set(-30*Math.cos(Math.PI/6),-30*Math.sin(Math.PI/6),0);
-  
   topleg2 = new THREE.Mesh(shape2, material);
   topleg2.rotation.z = Math.PI/2;
   topleg2.position.set(-30,0,0);
-  
   topleg1 = new THREE.Mesh(shape2, material);
   topleg1.rotation.z = Math.PI/3;
   topleg1.position.set(-30*Math.cos(Math.PI/6),30*Math.sin(Math.PI/6),0);
-  
+
   var spider = new THREE.Geometry();
   THREE.GeometryUtils.merge( spider, spbody );
   THREE.GeometryUtils.merge( spider, topleg1 );
@@ -59,6 +46,7 @@ function setup(){
   THREE.GeometryUtils.merge( spider, topleg6 );
 	
   spyRobot = new THREE.Mesh( spider );
+  spyRobot.applyMatrix(rotation);
   scene.add( spyRobot );
   scene.add(topleg6, topleg5, topleg4, topleg3, topleg2, topleg1, axisHelper, spbody);
   
@@ -74,6 +62,7 @@ function loop(){
   //camera.position.x = Math.cos( timer ) * 10;
   //camera.position.z = Math.sin( timer ) * 10;
   //camera.lookAt( scene.position );
+  camera.rotation.y += 0.01;
   requestAnimationFrame(loop);
   renderer.render(scene, camera);
 }
