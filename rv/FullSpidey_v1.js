043d38015262dc80c341dcce9ad850617ce02128
RSStiltskin <lvegacor@hotmail.com>
2016-05-31 07:47:53 -0500

    Update FullSpidey_v1.js

--- rv/FullSpidey_v1.js
+++ rv/FullSpidey_v1.js
@@ -9,8 +9,8 @@ function TopLLeg(){
   this.toplfoot = new THREE.Mesh(new THREE.ConeGeometry(4,20,20), new THREE.MeshBasicMaterial({color: 0x00FF80, wireframe: true}));
   //this.toplfoot.rotation.x=2*Math.PI/3;
   //this.toplfoot.rotation.y=Math.PI/3;
-  this.toplfoot.rotation.z=Math.PI/4;
-  this.toplfoot.position.set(-22*Math.cos(Math.PI/6),22*Math.sin(Math.PI/6),5);
+  this.toplfoot.rotation.z=2*Math.PI/3;
+  this.toplfoot.position.set(-22*Math.cos(Math.PI/6),22*Math.sin(Math.PI/6),0);
   this.toplleg.rotation.z=-2*Math.PI/3;
   this.toplleg.position.set(-8*Math.cos(Math.PI/6),8*Math.sin(Math.PI/6),0);
   this.unionlleg2.position.set(-16*Math.cos(Math.PI/6),16*Math.sin(Math.PI/6),0);
