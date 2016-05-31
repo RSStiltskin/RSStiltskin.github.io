93ae6bbbbc3a310c3ca59db3acf2033623ee6d91
RSStiltskin <lvegacor@hotmail.com>
2016-05-31 14:57:36 -0500

    Update FullSpidey_v5.js

--- rv/FullSpidey_v5.js
+++ rv/FullSpidey_v5.js
@@ -9,10 +9,10 @@ var LUlegrot = 2*Math.PI/3;
 
 function TopLLeg(){
   THREE.Object3D.call(this);
-  THREE.TextureLoader.crossOrigin='';
-  var texturelegs  =THREE.TextureLoader('https://raw.githubusercontent.com/RSStiltskin/RSStiltskin.github.io/master/spiderleg.jpg');
-  this.unionlleg = new THREE.Mesh( new THREE.SphereGeometry(8,20,20), new THREE.MeshBasicMaterial({map: texturelegs}) );
-  this.toplleg = new THREE.Mesh( new THREE.CylinderGeometry(8,4,16,15,10), new THREE.MeshBasicMaterial({map: texturelegs}) );
+  THREE.ImageUtils.crossOrigin = '';
+	this.texturelegs = THREE.ImageUtils.loadTexture('https://raw.githubusercontent.com/RSStiltskin/RSStiltskin.github.io/master/spiderleg.jpg');
+  this.unionlleg = new THREE.Mesh( new THREE.SphereGeometry(8,20,20), new THREE.MeshBasicMaterial({map: this.texturelegs}) );
+  this.toplleg = new THREE.Mesh( new THREE.CylinderGeometry(8,4,16,15,10), new THREE.MeshBasicMaterial({map: this.texturelegs}) );
   this.toplleg.rotation.z=-2*Math.PI/3;
   this.toplleg.position.set(-8*Math.cos(Math.PI/6),8*Math.sin(Math.PI/6),0);
   this.add(this.unionlleg, this.toplleg);
