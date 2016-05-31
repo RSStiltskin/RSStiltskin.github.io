15ddaf2f5ae4e2eda978c0b487c88ceac835c956
RSStiltskin <lvegacor@hotmail.com>
2016-05-30 20:39:08 -0500

    Update Spidey_v3.js

--- rv/Spidey_v3.js
+++ rv/Spidey_v3.js
@@ -3,6 +3,7 @@ var yleg = 20*Math.sin(Math.PI/6);
 
 var scene, camera, renderer;
 var step, mybody, upperlegL, upperlegR;
+var bottomlegR, bottomlegL;
 //var topleg, unionleg;
 //var spleg, cilleg, material1;
 
@@ -32,6 +33,28 @@ function TopRLeg(){
 }
 TopRLeg.prototype = new THREE.Object3D;
 
+function BottomRLeg(){
+  THREE.Object3D.call(this);
+  this.unionbrleg = new THREE.Mesh( new THREE.SphereGeometry(8,20,20), new THREE.MeshBasicMaterial({color:0xFFFF00, wireframe: true}) );
+  this.topbrleg = new THREE.Mesh( new THREE.CylinderGeometry(8,4,16,15,10), new THREE.MeshBasicMaterial({color:0x3ADF00, wireframe: true}) );
+  this.topbrleg.rotation.z=*Math.PI/3;
+  this.topbrleg.position.set(-8*Math.cos(Math.PI/6),8*Math.sin(Math.PI/6),0);
+  this.add(this.unionbrleg);
+  this.add(this.topbrleg);
+}
+BottomRLeg.prototype = new THREE.Object3D;
+
+function BottomLLeg(){
+  THREE.Object3D.call(this);
+  this.unionblleg = new THREE.Mesh( new THREE.SphereGeometry(8,20,20), new THREE.MeshBasicMaterial({color:0xFFFF00, wireframe: true}) );
+  this.topblleg = new THREE.Mesh( new THREE.CylinderGeometry(8,4,16,15,10), new THREE.MeshBasicMaterial({color:0x3ADF00, wireframe: true}) );
+  this.topblleg.rotation.z=-*Math.PI/3;
+  this.topblleg.position.set(-8*Math.cos(Math.PI/6),-8*Math.sin(Math.PI/6),0);
+  this.add(this.unionblleg);
+  this.add(this.topblleg);
+}
+BottomLLeg.prototype = new THREE.Object3D;
+
 function SpideyBody(){
   THREE.Object3D.call(this);
   this.fatbody = new THREE.Mesh( new THREE.SphereGeometry(20,20,20), new THREE.MeshBasicMaterial({color:0xFF0000, wireframe: true}) );
@@ -51,9 +74,15 @@ function setup(){
   //upperlegR.rotation.z = 2*Math.PI/3;
   upperlegR.position.set(20*Math.cos(Math.PI/6),20*Math.sin(Math.PI/6),0);
   
+  bottomlegL = new BottomLLeg;
+  bottomlegL.position.set(-20*Math.cos(Math.PI/6),-20*Math.sin(Math.PI/6),0);
+  
+  bottomlegR = new BottomRLeg;
+  bottomlegR.position.set(20*Math.cos(Math.PI/6),-20*Math.sin(Math.PI/6),0);
+  
   step = .01;
   scene = new THREE.Scene();
-  scene.add(mybody, upperlegL, upperlegR);
+  scene.add(mybody, upperlegL, upperlegR, bottomlegL, bottomlegR);
   camera = new THREE.PerspectiveCamera();
   camera.position.z=80;
  
