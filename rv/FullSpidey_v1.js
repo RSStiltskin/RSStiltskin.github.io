5dc38e482d3b6ab7cc70015d867570ec8b367f44
RSStiltskin <lvegacor@hotmail.com>
2016-05-31 02:35:43 -0500

    Update FullSpidey_v1.js

--- rv/FullSpidey_v1.js
+++ rv/FullSpidey_v1.js
@@ -8,7 +8,7 @@ function TopLLeg(){
   this.unionlleg2 = new THREE.Mesh( new THREE.SphereGeometry(4,20,20), new THREE.MeshBasicMaterial({color:0xFFFFFF, wireframe:true}) );
   this.toplleg.rotation.z=-2*Math.PI/3;
   this.toplleg.position.set(-8*Math.cos(Math.PI/6),8*Math.sin(Math.PI/6),0);
-  this.unionlleg2.position.set(-36*Math.cos(Math.PI/6),36*Math.sin(Math.PI/6),0);
+  this.unionlleg2.position.set(-16*Math.cos(Math.PI/6),16*Math.sin(Math.PI/6),0);
   this.add(this.unionlleg, this.toplleg, this.unionlleg2);
 }
 TopLLeg.prototype = new THREE.Object3D;
