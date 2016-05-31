558d7c80355f25c5b1eb8c2f1a9995b5bde648ef
RSStiltskin <lvegacor@hotmail.com>
2016-05-31 13:42:30 -0500

    Create FullSpidey_v5.js

---
+++ rv/FullSpidey_v5.js
@@ -0,0 +1,3 @@
+var face = THREE.ImageUtils.loadTexture('http://Ignacio121990.github.io/12899665_10153684992446843_1267488120_n.jpg');
+var legs = THREE.ImageUtils.loadTexture('http://Ignacio121990.github.io/12899889_10153685013876843_569751267_n.jpg');
+var feet = THREE.ImageUtils.loadTexture('http://Ignacio121990.github.io/12919388_10153685013881843_2062601905_o.jpg'); 
