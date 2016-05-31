70d235b9568fed4165017ccc29b95426f2013f60
RSStiltskin <lvegacor@hotmail.com>
2016-05-30 19:52:24 -0500

    Update Spidey_v3.js

--- rv/Spidey_v3.js
+++ rv/Spidey_v3.js
@@ -1,40 +1,50 @@
-var xleg1 = -20*Math.cos(Math.PI/6)-8*Math.cos(Math.PI/6);
-var yleg1 = 20*Math.sin(Math.PI/6)+8*Math.sin(Math.PI/6);
+var xleg = 20*Math.cos(Math.PI/6));
+var yleg = 20*Math.sin(Math.PI/6));
 
 var scene, camera, renderer;
-var step, body;
+var step, mybody, upperleg1, upperleg2;
 //var topleg, unionleg;
 //var spleg, cilleg, material1;
 
 //this.topleg.position.set( xleg1, yleg1 );
 //this.unionleg.position.set(-20*Math.cos(Math.PI/6),20*Math.sin(Math.PI/6),0);
 
-function TpLft(){
+//function TpLft(){
+function TopLeg(){
 THREE.Object3D.call(this);
-//this.spleg = new THREE.SphereGeometry(8,20,20);
-//this.cilleg = new THREE.CylinderGeometry(8,4,16,15,10);
-//this.material = new THREE.MeshBasicMaterial({color:0xFFFF00, wireframe: true});
 this.unionleg = new THREE.Mesh( new THREE.SphereGeometry(8,20,20), new THREE.MeshBasicMaterial({color:0xFFFF00, wireframe: true}) );
 this.topleg = new THREE.Mesh( new THREE.CylinderGeometry(8,4,16,15,10), new THREE.MeshBasicMaterial({color:0x3ADF00, wireframe: true}) );
-this.topleg.rotation.z=-2*Math.PI/3;
-this.topleg.position.set(-8*Math.cos(Math.PI/6),8*Math.sin(Math.PI/6),0);
+//this.topleg.rotation.z=-2*Math.PI/3;
+//this.topleg.position.set(-8*Math.cos(Math.PI/6),8*Math.sin(Math.PI/6),0);
 this.add(this.unionleg);
 this.add(this.topleg);
+}
+
+function SpideyBody(){
+  THREE.Object3D.call(this);
+  this.fatbody = new THREE.Mesh( new THREE.SphereGeometry(20,20,20), new THREE.MeshBasicMaterial({color:0xFF0000, wireframe: true}) );
+  this.add(this.fatbody);
 }
 
 TpLft.prototype = new THREE.Object3D;
 
 function setup(){
-body = new TpLft;
-step = .01;
-scene = new THREE.Scene();
-scene.add(body);
-camera = new THREE.PerspectiveCamera();
-camera.position.z=60;
-
-renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerHeight, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+  mybody = new SpideyBody;
+  upperleg1 = new TopLeg;
+  upperleg1.rotation.z = -2*Math.PI/3;
+  upperleg1.position.set(-8*Math.cos(Math.PI/6),8*Math.sin(Math.PI/6),0);
+  upperleg2 = new TopLeg;
+  upperleg2.rotation.z = 2*Math.PI/3;
+  upperleg2.position.set(8*Math.cos(Math.PI/6),8*Math.sin(Math.PI/6),0)
+  step = .01;
+  scene = new THREE.Scene();
+  scene.add(mybody, upperleg1, upperleg2);
+  camera = new THREE.PerspectiveCamera();
+  camera.position.z=80;
+ 
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerHeight, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 }
 
 function loop(){
