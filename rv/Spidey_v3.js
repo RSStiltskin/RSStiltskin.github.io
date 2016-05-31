ca47c40868c2725733e0ddfa556f5722371fc932
RSStiltskin <lvegacor@hotmail.com>
2016-05-30 20:27:45 -0500

    Update Spidey_v3.js

--- rv/Spidey_v3.js
+++ rv/Spidey_v3.js
@@ -2,7 +2,7 @@ var xleg = 20*Math.cos(Math.PI/6);
 var yleg = 20*Math.sin(Math.PI/6);
 
 var scene, camera, renderer;
-var step, mybody, upperleg1, upperleg2;
+var step, mybody, upperlegL, upperlegR;
 //var topleg, unionleg;
 //var spleg, cilleg, material1;
 
@@ -10,16 +10,27 @@ var step, mybody, upperleg1, upperleg2;
 //this.unionleg.position.set(-20*Math.cos(Math.PI/6),20*Math.sin(Math.PI/6),0);
 
 //function TpLft(){
-function TopLeg(){
+function TopLLeg(){
   THREE.Object3D.call(this);
-  this.unionleg = new THREE.Mesh( new THREE.SphereGeometry(8,20,20), new THREE.MeshBasicMaterial({color:0xFFFF00, wireframe: true}) );
-  this.topleg = new THREE.Mesh( new THREE.CylinderGeometry(8,4,16,15,10), new THREE.MeshBasicMaterial({color:0x3ADF00, wireframe: true}) );
-  //this.topleg.rotation.z=-2*Math.PI/3;
-  this.topleg.position.set(-8*Math.cos(Math.PI/6),8*Math.sin(Math.PI/6),0);
+  this.unionlleg = new THREE.Mesh( new THREE.SphereGeometry(8,20,20), new THREE.MeshBasicMaterial({color:0xFFFF00, wireframe: true}) );
+  this.toplleg = new THREE.Mesh( new THREE.CylinderGeometry(8,4,16,15,10), new THREE.MeshBasicMaterial({color:0x3ADF00, wireframe: true}) );
+  this.toplleg.rotation.z=-2*Math.PI/3;
+  this.toplleg.position.set(-8*Math.cos(Math.PI/6),8*Math.sin(Math.PI/6),0);
   this.add(this.unionleg);
   this.add(this.topleg);
 }
-TopLeg.prototype = new THREE.Object3D;
+TopLLeg.prototype = new THREE.Object3D;
+
+function TopRLeg(){
+  THREE.Object3D.call(this);
+  this.unionrleg = new THREE.Mesh( new THREE.SphereGeometry(8,20,20), new THREE.MeshBasicMaterial({color:0xFFFF00, wireframe: true}) );
+  this.toprleg = new THREE.Mesh( new THREE.CylinderGeometry(8,4,16,15,10), new THREE.MeshBasicMaterial({color:0x3ADF00, wireframe: true}) );
+  this.toprleg.rotation.z=-2*Math.PI/3;
+  this.toprleg.position.set(8*Math.cos(Math.PI/6),8*Math.sin(Math.PI/6),0);
+  this.add(this.unionleg);
+  this.add(this.topleg);
+}
+TopRLeg.prototype = new THREE.Object3D;
 
 function SpideyBody(){
   THREE.Object3D.call(this);
@@ -31,15 +42,18 @@ SpideyBody.prototype = new THREE.Object3D;
 
 function setup(){
   mybody = new SpideyBody;
-  upperleg1 = new TopLeg;
-  upperleg1.rotation.z = -2*Math.PI/3;
-  upperleg1.position.set(-xleg,yleg,0);
-  upperleg2 = new TopLeg;
-  upperleg2.rotation.z = 2*Math.PI/3;
-  upperleg2.position.set(xleg,yleg,0);
+  
+  upperlegL = new TopLLeg;
+  //upperlegL.rotation.z = -2*Math.PI/3;
+  upperlegL.position.set(-20*Math.cos(Math.PI/6),20*Math.sin(Math.PI/6),0);
+  
+  upperlegR = new TopRLeg;
+  //upperlegR.rotation.z = 2*Math.PI/3;
+  upperlegR.position.set(20*Math.cos(Math.PI/6),20*Math.sin(Math.PI/6),0);
+  
   step = .01;
   scene = new THREE.Scene();
-  scene.add(mybody, upperleg1, upperleg2);
+  scene.add(mybody, upperlegL, upperlegR);
   camera = new THREE.PerspectiveCamera();
   camera.position.z=80;
  
