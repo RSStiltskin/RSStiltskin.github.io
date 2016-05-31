eb6921c20c870d09c870cea72d81cb45d18b9634
RSStiltskin <lvegacor@hotmail.com>
2016-05-30 20:19:50 -0500

    Update Spidey_v3.js

--- rv/Spidey_v3.js
+++ rv/Spidey_v3.js
@@ -15,7 +15,7 @@ function TopLeg(){
   this.unionleg = new THREE.Mesh( new THREE.SphereGeometry(8,20,20), new THREE.MeshBasicMaterial({color:0xFFFF00, wireframe: true}) );
   this.topleg = new THREE.Mesh( new THREE.CylinderGeometry(8,4,16,15,10), new THREE.MeshBasicMaterial({color:0x3ADF00, wireframe: true}) );
   //this.topleg.rotation.z=-2*Math.PI/3;
-  //this.topleg.position.set(-8*Math.cos(Math.PI/6),8*Math.sin(Math.PI/6),0);
+  this.topleg.position.set(-8*Math.cos(Math.PI/6),8*Math.sin(Math.PI/6),0);
   this.add(this.unionleg);
   this.add(this.topleg);
 }
@@ -33,10 +33,10 @@ function setup(){
   mybody = new SpideyBody;
   upperleg1 = new TopLeg;
   upperleg1.rotation.z = -2*Math.PI/3;
-  upperleg1.position.set(-xleg-8*Math.cos(Math.PI/6),yleg+8*Math.sin(Math.PI/6),0);
+  upperleg1.position.set(-xleg,yleg,0);
   upperleg2 = new TopLeg;
   upperleg2.rotation.z = 2*Math.PI/3;
-  upperleg2.position.set(xleg+8*Math.cos(Math.PI/6),yleg+8*Math.sin(Math.PI/6),0)
+  upperleg2.position.set(xleg,yleg,0);
   step = .01;
   scene = new THREE.Scene();
   scene.add(mybody, upperleg1, upperleg2);
