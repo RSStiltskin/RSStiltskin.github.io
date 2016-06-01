4adf42bc07cdad5bd0d53dacc044d985f3619e6d
RSStiltskin <lvegacor@hotmail.com>
2016-06-01 13:33:48 -0500

    Update FullSpidey_v6.js

--- rv/FullSpidey_v6.js
+++ rv/FullSpidey_v6.js
@@ -269,14 +269,18 @@ LFoot.prototype = new THREE.Object3D;
   obstacle3 = raycaster3.intersectObject(Thewall3);
   obstacle4 = raycaster4.intersectObject(Thewall4);
   
-  fullbody.position.x += rotx;
-  //fullbody.position.y += roty;
+  //fullbody.position.x += rotx;
+  fullbody.position.y += roty;
 
   if((obstacle1.length > 0 && (obstacle1[0].distance <= 50))|| (obstacle2.length > 0 && (obstacle2[0].distance <= 50)))
   fullbody.rotation.z = rotz;
+  fullbody.position.x += rotx;
+  fullbody.position.y += roty;
 
   if((obstacle3.length > 0 && (obstacle3[0].distance <= 50))|| (obstacle4.length > 0 && (obstacle4[0].distance <= 50)))
   fullbody.rotation.z = rotz;
+  fullbody.position.x += rotx;
+  
 	
 //fullbody.rotation.z -= 0.005;
 	  fullbody.myoneleg.rotation.z += step1;
