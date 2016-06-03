cfec1056102edd901b130b2a9837fbfb5dd1fef2
RSStiltskin <lvegacor@hotmail.com>
2016-06-03 18:44:49 -0500

    Update pet_recycling.js

--- rv/pet_recycling.js
+++ rv/pet_recycling.js
@@ -5,7 +5,7 @@ function A1(){
   this.a1 = new THREE.Mesh( new THREE.SphereGeometry(20,20,20), new THREE.MeshBasicMaterial({color:0xFFFF00, wireframe: true}) );
   this.add(this.a1);
 }
-TopLLeg.prototype = new THREE.Object3D;
+A1.prototype = new THREE.Object3D;
 
 function setup(){
   Proof = new A1;
