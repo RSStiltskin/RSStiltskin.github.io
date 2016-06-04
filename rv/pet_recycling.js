846214967a49b654f362b1d48f6661c0f9795a16
RSStiltskin <lvegacor@hotmail.com>
2016-06-03 19:56:20 -0500

    Update pet_recycling.js

--- rv/pet_recycling.js
+++ rv/pet_recycling.js
@@ -7,13 +7,31 @@ function A1(){
 }
 A1.prototype = new THREE.Object3D;
 
+function conveyor(){
+  THREE.Objet3D.call(this);
+  this.C1 = new THREE.Mesh( new THREE.BoxGeometry(20,1,7), new THREE.MeshNormalMaterial());
+  this.C2 = new THREE.Mesh( new THREE.BoxGeometry(20,1,7), new THREE.MeshNormalMaterial());
+  this.C3 = new THREE.Mesh( new THREE.BoxGeometry(20,20,5), new THREE.MeshNormalMaterial());
+  this.C1.position.y = -10;
+  this.C2.position.y = 10;
+  this.add(this.C1, this.C2, this.C3);
+}
+conveyor.prototype = new THREE.Object3D;
+
+
 function setup(){
-  Proof = new A1;
+  con1 = new conveyor;
+  con1.position.x = -20;
+  con2 = new conveyor;
+  con2.position.x =-10;
+  con2.rotation.y = Math.PI/4;
+  con3 = new conveyor;
+  con3.position.z = 20*Math.sin(Math.PI/4); 
   scene = new THREE.Scene();
-  scene.add(Proof);
+  scene.add(con1, con2, con3);
   camera = new THREE.PerspectiveCamera(100, window.innerWidth/window.innerHeight,1,1000);
-  camera.position.z=80;
- 
+  camera.position.set(20,20,80);
+  
   renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
