5ef9fd08547f08070752c330c4978c8245cbfc6a
RSStiltskin <lvegacor@hotmail.com>
2016-06-05 14:03:13 -0500

    Update FullSpidey_v6.js

--- rv/FullSpidey_v6.js
+++ rv/FullSpidey_v6.js
@@ -250,53 +250,11 @@ function loop(){
   requestAnimationFrame( loop );
   camera.lookAt(scene.position);
   obstacle1 = raycaster1.intersectObject(floyd);
-  //obstacle2 = raycaster2.intersectObject(Thewall2);
-  //obstacle3 = raycaster3.intersectObject(Thewall3);
-  //obstacle4 = raycaster4.intersectObject(Thewall4);
-  
-  //fullbody.position.y += roty;
-  //fullbody.position.x += rotx;
-  
-  //obstaculo1=obs.raycaster.intersectObjects(escena.children,true);
+  fullbody.position.y += step;
   if((obstacle1.length>0&&(obstacle1[0].distance<=50))){
-   if(coor==1){
-    fullbody.rotation.z = rotz;
-    //obs.rob.rotation.y+=Math.PI/2;
-    raycaster1.set( fullbody.position, new THREE.Vector3(1,0,0) );
-    //obs.raycaster.set(obs.rob.position,new THREE.Vector3(0,0,-1));
-    coor=2;}
-  else if(coor==2){
-   fullbody.rotation.z = rotz;
-   raycaster1.set( fullbody.position, new THREE.Vector3(0,-1,0) );
-   //obs.rob.rotation.y+=Math.PI/2;
-   //obs.raycaster.set(obs.rob.position,new THREE.Vector3(-1,0,0));
-   coor=3;}
-  else if(coor==3){
-   fullbody.rotation.z = rotz;
-   raycaster1.set( fullbody.position, new THREE.Vector3(-1,0,0) );
-   //obs.raycaster.set(obs.rob.position,new THREE.Vector3(0,0,1));
-   coor=4;}
-  else if(dir==4){
-   fullbody.rotation.z = rotz;
-   raycaster1.set( fullbody.position, new THREE.Vector3(0,1,0) );
-   //obs.rob.rotation.y+=Math.PI/2;
-   //obs.raycaster.set(obs.rob.position,new THREE.Vector3(1,0,0));
-   coor=1;}
-}
-if(coor==1){
-fullbody.position.y += step;
-}
-else if(coor==2){
-fullbody.position.x += step; 
-}
-else if(coor==3){
-fullbody.position.y -= step; 
-}
-else if(coor==4){
-fullbody.position.x -= step; 
-}
-  renderer.render (scene, camera);
-}
+  	fullbody.rotation.z = Math.PI;
+  	fullbody.position.y += -step;}
+  
 	
 setup();
 loop();
