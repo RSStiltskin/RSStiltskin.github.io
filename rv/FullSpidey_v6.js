07ecfc325700f0596689755ed5e3fe312b8b58f7
RSStiltskin <lvegacor@hotmail.com>
2016-06-05 23:07:00 -0500

    Update FullSpidey_v6.js

--- rv/FullSpidey_v6.js
+++ rv/FullSpidey_v6.js
@@ -258,34 +258,18 @@ function loop(){
   //fullbody.position.y += step;
   if((obstacle3.length>0&&(obstacle3[0].distance<=50))){
   	fullbody.rotation.z += Math.PI/2;
-  	coor=2;
-  	raycaster1.set(fullbody.position, new THREE.Vector3(0,1,0));
-  	raycaster2.set(fullbody.position, new THREE.Vector3(-1,0,0));
-  	raycaster3.set(fullbody.position, new THREE.Vector3(0,1,0));
-  	raycaster4.set(fullbody.position, new THREE.Vector3(0,-1,0));}
+  	coor=2;}
   
   if ((obstacle4.length>0&&(obstacle4[0].distance<=50))){
   	fullbody.rotation.z += Math.PI/2;
-  	coor=3;
-  	raycaster1.set(fullbody.position, new THREE.Vector3(0,1,0));
-  	raycaster2.set(fullbody.position, new THREE.Vector3(-1,0,0));
-  	raycaster3.set(fullbody.position, new THREE.Vector3(0,1,0));
-  	raycaster4.set(fullbody.position, new THREE.Vector3(0,-1,0));}
+  	coor=3;}
   	
   if ((obstacle1.length>0&&(obstacle1[0].distance<=50))){
 	fullbody.rotation.z += Math.PI/2;
-	coor=4;
-  	raycaster1.set(fullbody.position, new THREE.Vector3(0,1,0));
-  	raycaster2.set(fullbody.position, new THREE.Vector3(-1,0,0));
-  	raycaster3.set(fullbody.position, new THREE.Vector3(0,1,0));
-  	raycaster4.set(fullbody.position, new THREE.Vector3(0,-1,0));}
+	coor=4;}
   if ((obstacle2.length>0&&(obstacle2[0].distance<=50))){
   	fullbody.rotation.z += Math.PI/2;
-  	coor=1;
-  	raycaster1.set(fullbody.position, new THREE.Vector3(0,1,0));
-   	raycaster2.set(fullbody.position, new THREE.Vector3(-1,0,0));
-	raycaster3.set(fullbody.position, new THREE.Vector3(0,1,0));
-  	raycaster4.set(fullbody.position, new THREE.Vector3(0,-1,0));}
+  	coor=1;}
  
   
   if(coor==1){fullbody.position.y += step;}
@@ -293,10 +277,10 @@ function loop(){
   if(coor==2){fullbody.position.x += step;}
   if(coor==3){fullbody.position.x -= step;}
   	
-  //raycaster1.set(fullbody.position, new THREE.Vector3(0,1,0));
-  //raycaster2.set(fullbody.position, new THREE.Vector3(-1,0,0));
-  //raycaster3.set(fullbody.position, new THREE.Vector3(0,1,0));
-  //raycaster4.set(fullbody.position, new THREE.Vector3(0,-1,0));
+  raycaster1.set(fullbody.position, new THREE.Vector3(0,1,0));
+  raycaster2.set(fullbody.position, new THREE.Vector3(-1,0,0));
+  raycaster3.set(fullbody.position, new THREE.Vector3(0,1,0));
+  raycaster4.set(fullbody.position, new THREE.Vector3(0,-1,0));
   renderer.render (scene, camera);
 }
 setup();
